1297c6f3de01b71f1779d2a88a2f3022b641abcf
John Paul Andersen <neutronstar@gmx.us>
2019-02-12 19:26:10 -0700

    Removed some unnecessary components and started switching to new API

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
 
 document.getElementById("weatherSubmit").addEventListener("click", function(event) {
   event.preventDefault();
-  const value = document.getElementById("weatherInput").value;
+  let value = document.getElementById("weatherInput").value;
   if (value === "")
     return;
   //console.log(value);
 
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=8238befcdc132d4bde8142ab1f63927e";
+  //const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=8238befcdc132d4bde8142ab1f63927e";
+
+  value = value.split(' ').join('+');
+
+  console.log(value);
+
+  //const url = "https://tastedive.com/api/similar?q=" + value + "&type=music&info=1&k=330172-FindMeMu-V539BPIE";
+
+  const url = "https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction";
+
+  console.log(url);
+
   fetch(url)
    .then(function(response) {
-     //console.log(response);
+     console.log(response);
      return response.json();
    }).then(function(json) {
      console.log(json);
@@ -62,65 +73,7 @@ results += "<p>    </p>";
 
      document.getElementById("weatherResults").innerHTML = results;
    });
-   const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=8238befcdc132d4bde8142ab1f63927e";
-   fetch(url2)
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      console.log(json);
-      let forecast = "";
-      let tempForecast = "";
-
-      var current = "";
-
-      for (let i=0; i < json.list.length; i++) {
-        var variable =  moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm a');
-
-        //var variable = forecast;
-
-        console.log(variable);
-
-        var parts = variable.split(", ");
-
-        if (parts[0] != current) {
-          if (current != "") {
-            forecast += "</div>";
-          }
-          forecast += "<b><h1>" + parts[0] + "</h1></b>";
-          forecast += "<div class=\"scroller\">";
-        }
-        forecast += "<div style=\"display: inline-block; margin-left: 30px; margin-right: 30px; text-align: start; \">";
-        forecast += "<h4>" + parts[1] +  "</h4>";
-
-        forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png" height=\"45\" width=\"45\" />';
-
-        //forecast += "</div>";
-        //forecast += "<div style=\"float: left;\">"
-        //forecast += "<div>" + parseInt(json.list[i].main.temp) + "°F" + "</div>";
-        //forecast += "<div style=\"display: block; width: 85px; overflow: visible;\"></div>";
-       //forecast += "</div>";
-
-
-        forecast += "<h5>" + parseInt(json.list[i].main.temp) + "°F" + "</h5>";
-        //forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
-
-        forecast += "<div style=\"display: block; clear: left; font-size: x-small;\">Clouds: " + json.list[i].clouds.all + "%</div>";
-        forecast += "<div style=\"display: block; clear: left; color: gray; font-size: x-small;\" >Humidity: " + json.list[i].main.humidity + "%</div>";
-
-        forecast += "</div>";
-
-        if (parts[0] != current) {
-          current = parts[0];
-        }
-
-      }
-
-      forecast += "</div>";
-      document.getElementById("forecastResults").innerHTML = forecast;
-    });
 });
-
-document.getElementById("forecast").addEventListener("click", openButton);
 
 function openButton(event) {
   this.classList.toggle("active");
